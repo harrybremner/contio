1f693f03b07698149d7c49f5dd39f5d9e8f18e23
Harry Bremner <harrybremner@gmail.com>
2022-06-09 13:08:16 +0100

    some changes to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-
+  def home
+    @user = current_user
+    if @user.is_contractor
+      @projects = Project.where(contractor: @user)
+    else
+      @projects = Project.where(client: @user)
+    end
+  end
 
 end
