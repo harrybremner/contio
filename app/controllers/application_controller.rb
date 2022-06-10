8fe88270fb342344d926b8a80d8850d056929b82
Harry Bremner <harrybremner@gmail.com>
2022-06-10 13:39:07 +0100

    finished some more inbox stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :skip_footer
-  
+
   def skip_footer
-    @hide_mobile_footer = true
+    # @hide_mobile_footer = true
   end
 
   def configure_permitted_parameters
