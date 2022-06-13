803f64b78d4ff5b5c2efbd3bb79ca82f17b83d95
Anthony Carter <altvnko@gmail.com>
2022-06-13 15:40:44 +0100

    progress bar logic

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,12 +13,11 @@ application.register("mobile-navbar", MobileNavbarController)
 import ProjectSubscriptionController from "./project_subscription_controller.js"
 application.register("project-subscription", ProjectSubscriptionController)
 
+import Select2Controller from "./select2_controller.js"
+application.register("select2", Select2Controller)
 
 import SubTasksController from "./sub_tasks_controller.js"
 application.register("sub-tasks", SubTasksController)
 
-import Select2Controller from "./select2_controller.js"
-application.register("select2", Select2Controller)
-
-
-
+import TaskProgressController from "./task_progress_controller.js"
+application.register("task-progress", TaskProgressController)
