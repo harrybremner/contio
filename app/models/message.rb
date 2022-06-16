31d2e9de3552931927c2e7aec6e1d7392da81b45
Harry Bremner <harrybremner@gmail.com>
2022-06-16 10:52:18 +0100

    adding alerts to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,11 @@ class Message < ApplicationRecord
   validates :content, presence: true
   has_many_attached :files
 
+
+  # after_commit :send_notification
+
+
+  # def send_notifcation
+  # end
+
 end
